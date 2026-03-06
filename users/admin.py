6ac973596392f25464c9219a4d2643ee1f9fca97
gm-willo <gianmarco.ninno@willo.it>
2026-03-06 17:42:04 +0100

    refactor: Add _str_ method to User model and customize fields displayed on the 'add' and 'changes' user pages in the admin site

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = [
+        "username",
+        "is_active",
+        "is_superuser",
+        "last_login",
+        "date_joined",
+    ]
+    list_filter = ["is_active", "is_superuser", "last_login"]
+
+    fieldsets = [
+        (None, {"fields": ["username", "password"]}),
+        ("Authorization", {"fields": ["is_active", "is_staff", "is_superuser"]}),
+    ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": [
+                    "wide",
+                ],
+                "fields": ["username", "password1", "password2", "is_staff"],
+            },
+        ),
+    ]
