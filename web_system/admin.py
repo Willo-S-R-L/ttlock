7cdc6bf2603f16d6769fd43bd0271aa92a7e8047
gm-willo <gianmarco.ninno@willo.it>
2026-03-06 17:55:24 +0100

    feat: Create TTLockToken model needed to make TTLock API requests, create Lock model to match locks to their owners and customize fields showed in the lock pages in the admin site

--- web_system/admin.py
+++ web_system/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Lock
+
+@admin.register(Lock)
+class LockAdmin(admin.ModelAdmin):
+    list_display = [
+        "ttlock_id"
+    ]
+    search_fields = ["ttlock_id"]
+    list_filter = ["created_at"]
+
+    readonly_fields = [
+        "ttlock_id",
+        "owner",
+        "created_at",
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def get_actions(self, request):
+        return super().get_actions(request)
