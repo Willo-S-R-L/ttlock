e5262fef9cb993388ba3d1f220549b9ee6217a8d
gm-willo <gianmarco.ninno@willo.it>
2026-03-27 17:43:59 +0100

    feat: Add new url path for the passcode creation and refactor the lock sync path to follows best practices

--- web_system/urls.py
+++ web_system/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("lock/sync/", views.sync_locks, name="sync_locks")
+    path("lock/sync/", views.lock_sync, name="lock_sync"),
+    path("lock/<int:lock_id>/passcode/", views.passcode_add, name="passcode_add"),
 ]
